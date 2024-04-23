6178a3266dd95516f703a4cd4d8741ce04f48386
Angelo Kusuma <84068505+Akusuma05@users.noreply.github.com>
2024-04-23 22:48:59 +0700

    Update Home

--- lib/view/pages/Home.dart
+++ lib/view/pages/Home.dart
@@ -8,46 +8,97 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: SafeArea(
-                child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  child: const Icon(FeatherIcons.user),
-                ),
-                Column(
-                  children: [
-                    Container(
-                      // width: double.infinity,
-                      child: const Text(
-                        "Good Morning Angelo",
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    Container(
-                      // width: double.infinity,
-                      child: const Text(
-                        "070601201026",
-                        textAlign: TextAlign.left,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            )),
+          SafeArea(
+            child: Column(
+              children: [_buildUserInfo(), _bottomSheet()],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Padding _buildUserInfo() {
+    return Padding(
+      padding: const EdgeInsets.only(
+          bottom: 32, top: 16, left: 8, right: 8), // Add this line
+      child: Row(
+        children: [
+          Icon(
+            FeatherIcons.user,
+            size: 40,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Good Morning Angelo",
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 6),
+              Text(
+                "070601201026",
+                textAlign: TextAlign.left,
+              )
+            ],
           )
         ],
       ),
+    );
+  }
+
+  Expanded _bottomSheet() {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          border: Border.all(color: Colors.transparent),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+        ),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0), // Add this line
+              child: Text(
+                "Classes",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  NavigationBar _buildBottomNavigationBar() {
+    return NavigationBar(
+      height: 80,
+      elevation: 0,
+      selectedIndex: _selectedIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      destinations: const [
+        NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+        NavigationDestination(icon: Icon(Icons.person), label: "Profile")
+      ],
     );
   }
 }
--- lib/view/pages/Pages.dart
+++ lib/view/pages/Pages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 part 'Login.dart';
 part 'Register.dart';
